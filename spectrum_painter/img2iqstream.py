d7cdd54761ee1384db36bb9ca6ea452c48578848
Yoav Zangvil <yoav.zangvil@gmail.com>
2020-06-16 10:50:04 +0300

    modified to support python 3.6 and scipy 1.4. Need to install imageio

--- spectrum_painter/img2iqstream.py
+++ spectrum_painter/img2iqstream.py
@@ -7,7 +7,7 @@ FORMATTERS = {'float': GenericFloat, 'bladerf': Bladerf, 'hackrf': Hackrf}
 @click.command()
 @click.option('--samplerate', '-s', type=int, default=1000000, help='Samplerate of the radio')
 @click.option('--linetime', '-l', type=float, default=0.005, help='Time for each line to show')
-@click.option('--output', '-o', type=click.File('w'), help='File to write to (default: stdout)', default='-')
+@click.option('--output', '-o', type=click.File('wb'), help='File to write to (default: stdout)', default='-')
 @click.option('--format', type=click.Choice(['float', 'bladerf', 'hackrf']), default='float', help='Output format of samples')
 @click.argument('srcs', nargs=-1, type=click.Path(exists=True))
 def img2iqstream(samplerate, linetime, output, format, srcs):
