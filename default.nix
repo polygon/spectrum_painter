2f1820160c594b6074257b3ef2c173fa97f481c8
Jan Dohl <jan@legxaro.de>
2021-05-12 22:52:40 +0200

    Fully switch over to flakes

--- default.nix
+++ default.nix
@@ -1,22 +1,8 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs {}
-}:
-with pkgs; 
-let
-  python3-with-deps = python3.withPackages (ps: with ps; [
-    click
-    numpy
-    imageio
-  ]);
-in
-python3.pkgs.buildPythonPackage rec {
-  pname = "spectrum_painter";
-  version = "0.1.0";
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
 
-  src = ./.;
-  doCheck = false;
-  
-  buildInputs = [
-    python3-with-deps
-  ];
-}
